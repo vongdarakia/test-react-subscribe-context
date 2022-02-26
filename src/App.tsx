809d32c185a67b4086880a60217794051aff8797
Akia Vongdara <vongdarakia@gmail.com>
2022-02-26 12:15:49 -0800

    Remove react tracked demo: not IE compatible

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 import { DeepSubscriberDemo } from "./examples/DeepSubscriberDemo/DeepSubscriberDemo";
-import { ReactTrackDemo } from "./examples/ReactTrackDemo/ReactTrackDemo";
 
 function App() {
     return (
         <div className="App">
             <DeepSubscriberDemo />
-            <ReactTrackDemo />
         </div>
     );
 }
